40560177f490c3176ffe2836c0c308114e0414d5
_xiaotian <952822399@qq.com>
2018-08-24 00:28:17 +0800

    Initial commit

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'e-video'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
