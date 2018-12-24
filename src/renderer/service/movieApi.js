2fe2de88b45bc5429f0ef472a70665b58744470b
_xiaotian <952822399@qq.com>
2018-12-24 23:16:07 +0800

    删除注释

--- src/renderer/service/movieApi.js
+++ src/renderer/service/movieApi.js
@@ -1,13 +1,7 @@
 import Api from './Api'
 
 export default {
-  /**
-   * 默认30条数据，可以自定义
-   * @param limit
-   * @returns {*}
-   */
   getTopMv (params) {
-    // return Api().get(`/mv/first?limit=${limit}`)
     return Api().get('/top/mv', {
       params
     })
